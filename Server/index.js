be3e14315a1c557ff6e737574126493be9d90832
Esteban Sayago <sayagoo823@gmail.com>
2024-02-12 15:50:35 -0300

    create connection to db

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import routes from './src/routes/test.route.js';
+import mongoose from 'mongoose';
+import config from './src/config/config.js';
 
 const app = express();
 
-app.use('/', routes);
+mongoose.connect(config.db.mongodbUri).then(function () {
+  console.log('connection to database successful!');
+  app.use('/', routes);
 
-app.get('/', function (_, res) {
-  res.send('hello world');
-});
+  app.get('/', function (_, res) {
+    res.send('hello world');
+  });
 
-app.listen(3000, function () {
-  console.log('🔥 Server is running at port 3000');
+  app.listen(3000, function () {
+    console.log('🔥 Server is running at port 3000');
+  });
 });
