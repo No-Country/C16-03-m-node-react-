6a5fcce9e5c32dadc86ae24d686652cdb08788ce
JordanES <jordan11.tauro@gmail.com>
2024-02-12 17:01:20 -0500

    Create Users model with validation

---
+++ Server/src/models/Users.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const Schema = mongoose.Schema;
+
+export const usersSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+  },
+});
+
