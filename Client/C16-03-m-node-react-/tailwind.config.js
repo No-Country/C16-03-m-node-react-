10d678de6d5e87de77b8b0b6248477017b908179
Your Name <you@example.com>
2024-02-12 13:44:31 -0500

    style: tailwind config
    fonts, size fonts, breakpoints, colors

--- Client/C16-03-m-node-react-/tailwind.config.js
+++ Client/C16-03-m-node-react-/tailwind.config.js
@@ -2,6 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    fontSize: {
+      sm: "18px",
+      md: "23px",
+      lg: "30px",
+      xl: "60px",
+    },
+    colors: {
+      Amethyst: "#916ACC",
+      black: "#000000",
+      white: "#FFFFFF",
+      pink: "#F08898",
+      green: "#8BEA00",
+      purple: "#421A80",
+    },
+    screens: {
+      sm: "600px",
+      ms: "905px",
+      md: "1240px",
+      lg: "1400px",
+    },
     extend: {},
   },
   plugins: [],
