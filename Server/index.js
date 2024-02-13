c5b022ef38d000cc829007b7dbcf5030303cc15a
JordanES <jordan11.tauro@gmail.com>
2024-02-13 14:40:21 -0500

    add auth routes and JSON body parsing to index.js

--- Server/index.js
+++ Server/index.js
@@ -2,17 +2,19 @@ import express from 'express';
 import routes from './src/routes/test.route.js';
 import mongoose from 'mongoose';
 import config from './src/config/config.js';
+import authRoutes from './src/routes/auth.routes.js';
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(config.db.mongodbUri).then(function () {
   console.log('connection to database successful!');
   app.use('/', routes);
-
   app.get('/', function (_, res) {
     res.send('hello world');
   });
 
+  app.use('/auth', authRoutes);
   app.listen(3000, function () {
     console.log('🔥 Server is running at port 3000');
   });
