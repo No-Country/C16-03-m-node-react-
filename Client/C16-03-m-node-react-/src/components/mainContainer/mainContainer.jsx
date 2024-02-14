0e62acc0a7e4e30241dae7694e95147b606975db
German Ravarotto <g44ravarotto@gmail.com>
2024-02-14 13:03:11 -0300

    resposive done

--- Client/C16-03-m-node-react-/src/components/mainContainer/mainContainer.jsx
+++ Client/C16-03-m-node-react-/src/components/mainContainer/mainContainer.jsx
@@ -1,12 +1,35 @@
 
-
+import TextLanding from "../textLanding/TextLanding"
+import Logo from "../logo/Logo"
+import LandingImage from "../landingImage/Image"
+import TextInput from "../textInput/TextInput"
+import Button from "../button/Button"
+// import Rating from "../rating/Rating"
 
 function MainContainer() {
     return (
-        <div className="lg:w-[1440] lg:h-[1024] md:w-[1240] md:h-[1360] ms:w-[905] ms:h-[1300] sm:w-[430] sm:h-[577] flex justify-between items-center bg-purple">
-            
-            {/* Agregar todos los componentes */}
-        </div>
+        <main className="w-full h-[100vh] flex justify-between items-center bg-purple flex-wrap">
+            <header className="w-full p-[25px] flex justify-between items-center">
+                <Logo />
+                <Button text="Clientes" bgcolor="bg-green" />
+            </header>
+            <section className="w-full sm:flex sm:flex-row flex flex-col p-[25px] gap-2 justify-between items-center ">
+                <div>
+                    <TextLanding />
+                </div>
+                <div>
+
+                <LandingImage />
+                </div>
+            </section>
+            <section className="w-full p-[25px] gap-3 flex  items-center">
+                <TextInput />
+                <Button text="buscar" />
+            </section>
+            {/* <footer>
+                <Rating/>
+            </footer> */}
+        </main>
     );
 }
 
