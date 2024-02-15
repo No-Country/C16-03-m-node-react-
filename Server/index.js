ff741b47eaec18c86a1bf4c0349e33af10bc0d3f
Carlos A. Escobar <carlosesconav@gmail.com>
2024-02-15 15:33:45 -0500

    remove conflicts

--- Server/index.js
+++ Server/index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import routes from './src/routes/test.route.js';
 import mongoose from 'mongoose';
 import config from './src/config/config.js';
-import productRoutes from './src/routes/product.route.js';
 
 const app = express();
-app.use(express.json());
 
 mongoose.connect(config.db.mongodbUri).then(function () {
   console.log('connection to database successful!');
@@ -15,7 +13,6 @@ mongoose.connect(config.db.mongodbUri).then(function () {
     res.send('hello world');
   });
 
-  app.use('/product', productRoutes);
   app.listen(3000, function () {
     console.log('🔥 Server is running at port 3000');
   });
