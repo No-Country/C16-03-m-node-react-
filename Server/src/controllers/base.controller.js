98ff9529d435c5d80bd0ff185911e9c0c74b975a
Esteban Sayago <sayagoo823@gmail.com>
2024-02-20 18:56:05 -0300

    create new controllers

--- Server/src/controllers/base.controller.js
+++ Server/src/controllers/base.controller.js
@@ -3,42 +3,47 @@ import { BaseSchema } from '../models/Base.js';
 
 const Base = mongoose.model('Base', BaseSchema);
 
-class BaseController {
-  async create(req, res) {
-    try {
-      const base = new Base({
-        ...req.body,
-      });
-      await base.save();
-      res.status(200).json(base);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  }
-
-  async edit(req, res) {
-    const { id } = req.params;
-    try {
-      const editedBase = await Base.findByIdAndUpdate(
-        id,
-        { ...req.body },
-        { runValidators: true, returnDocument: 'after' },
-      );
-      res.status(200).json(editedBase);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  }
-
-  async delete(req, res) {
-    const { id } = req.params;
-    await Base.findByIdAndDelete(id);
-    res.status(200).json({ message: 'base deleted' });
-  }
-
-  test(req, res) {
-    res.send('pasate por el middleware');
+async function create(req, res) {
+  try {
+    const base = new Base({
+      ...req.body,
+    });
+    await base.save();
+    res.status(200).json(base);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 }
 
-export default new BaseController();
+async function edit(req, res) {
+  const { id } = req.params;
+  try {
+    const editedBase = await Base.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { runValidators: true, returnDocument: 'after' },
+    );
+    res.status(200).json(editedBase);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
+
+async function deleteOne(req, res) {
+  const { id } = req.params;
+  await Base.findByIdAndDelete(id);
+  res.status(200).json({ message: 'base deleted' });
+}
+
+async function getAll(req, res) {
+  const bases = await Base.find({});
+  return res.status(200).json(bases);
+}
+
+async function getOne(req, res) {
+  const { id } = req.params;
+  const base = await Base.findById(id);
+  return res.status(200).json(base);
+}
+
+export default { create, edit, deleteOne, getAll, getOne };
