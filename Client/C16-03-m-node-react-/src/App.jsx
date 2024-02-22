6fbe3741b8c4b59ae1f6caf1c8c6e2f32e5b451b
German <g44ravarotto@gmail.com>
2024-02-22 16:20:07 -0300

    Fixed responsive table

--- Client/C16-03-m-node-react-/src/App.jsx
+++ Client/C16-03-m-node-react-/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Login from "./components/login/Login";
+
 import DashboardClient from "./views/DashboardClient";
 import Landing from "./views/Landing";
 
--- Client/C16-03-m-node-react-/src/components/tableDetails/TableItems.jsx
+++ Client/C16-03-m-node-react-/src/components/tableDetails/TableItems.jsx
@@ -1,10 +1,10 @@
 const TableItems = ({ items, states }) => {
   return (
-    <div className="w-full h-full flex lg:flex-row sm:flex-col min-[320px]:flex-col  items-center justify-center">
+    <div className="w-full h-full flex lg:flex-row sm:flex-col min-[320px]:flex-col lg:items-center lg:justify-center">
       {items.map((item) => (
         <div
           key={item.id}
-          className="w-full  flex lg:flex-row sm:flex-col min-[320px]:flex-col min-[320px]:text-xs items-center justify-center lg:text-lg gap-10 sm:text-sm text-black"
+          className="w-full  flex lg:flex-row sm:flex-col min-[320px]:flex-col min-[320px]:text-xs items-center justify-center lg:text-md gap-10 sm:text-sm text-black"
         >
           {Object.entries(item).map(([key, value]) => {
             const stateColor = states[0][value];
