ad264b54eaf3ed6b5cf2638ca499adfa1f364aa2
German <g44ravarotto@gmail.com>
2024-02-26 12:02:07 -0300

    width changes

--- Client/C16-03-m-node-react-/src/views/DashboardVisitor.jsx
+++ Client/C16-03-m-node-react-/src/views/DashboardVisitor.jsx
@@ -5,9 +5,9 @@ import Table from "../components/tableDetails/Table";
 
 function DashboardVisitor() {
   return (
-    <div className="h-[100vh]  flex flex-col p-2 gap-2 md:gap-4 bg-purpleDark">
+    <div className="h-[100vh]  flex flex-col p-2 gap-2 md:gap-4 items-center bg-purpleDark">
       <Header />
-      <div className="flex flex-col mt-2 h-full sm:flex-row gap-5 rounded-[24px] bg-Amethyst">
+      <div className="flex flex-col mt-2 h-full sm:flex-row gap-5 rounded-[24px] lg:w-3/4 sm:w-3/4 min-[360px]:w-full  bg-Amethyst">
         <div className="flex flex-col justify-center gap-5 w-full">
           <StatusBar />
           <Table />
