9d4d9603d5664a3bd685e8e96b373510f4d7e07b
German <g44ravarotto@gmail.com>
2024-02-26 09:58:33 -0300

    changes buttons

--- Client/C16-03-m-node-react-/src/views/DashboardClient.jsx
+++ Client/C16-03-m-node-react-/src/views/DashboardClient.jsx
@@ -10,21 +10,12 @@ function DashboardClient() {
     <main className="w-full h-[100vh] ">
       <div className=" w-full lg:h-full sm:h-full min-[320px]:h-auto flex flex-col p-2 bg-Dark">
         <Header />
-        <div className=" w-full h-auto lg:hidden sm:hidden items-start justify-center gap-5 p-5 min-[360px]:flex ">
-          <Button text="Nuevo envío" bgcolor="bg-green" />
-          <Button text="Nuevo envío" bgcolor="bg-green" />
-          <Button text="Nuevo envío" bgcolor="bg-green" />
-        </div>
         <div className="w-full h-full flex flex-col sm:flex-row gap-3">
           <div className="lg:w-1/4 sm:w-1/2 h-full min-[360px]:w-full">
             <MyShipments />
           </div>
           <div className="flex flex-col gap-5 lg:w-3/4 sm:w-1/2 min-[360px]:w-full h-full justify-around bg-Amethyst rounded-lg  ">
-            <div className=" w-full h-auto lg:flex sm:flex items-start justify-center gap-5 p-5 min-[360px]:hidden ">
-              <Button text="Nuevo envío" bgcolor="bg-green" />
-              <Button text="Editar envío" bgcolor="bg-green" />
-              <Button text="Cancelar envío" bgcolor="bg-green" />
-            </div>
+
             <div className="w-full h-full flex flex-col justify-around rounded-lg">
               <StatusBar />
               <Table />
