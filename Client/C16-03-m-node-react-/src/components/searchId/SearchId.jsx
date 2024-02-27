46b527a2bc2600370aa36c378ec0680b748c3021
German <g44ravarotto@gmail.com>
2024-02-27 15:05:06 -0300

    validation and errors ID

--- Client/C16-03-m-node-react-/src/components/searchId/SearchId.jsx
+++ Client/C16-03-m-node-react-/src/components/searchId/SearchId.jsx
@@ -5,34 +5,34 @@ import { useNavigate } from "react-router-dom";
 function SearchId() {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState("");
+  const [errorId, setErrorId] = useState(false);
 
   const onSubmit = (event) => {
     event.preventDefault();
+    const inputValue = event.target.idNumber.value;
     if (inputValue.trim() !== "") {
       navigate("/dashboard-user/" + inputValue);
     } else {
-      // Aquí puedes agregar lógica para manejar el caso en que el campo esté vacío
-      alert("ERROR");
+      setErrorId(true);
     }
   };
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
   return (
-    <form onSubmit={onSubmit} className="space-x-3">
-      <TextInput
-        placeholdertext={"Ingresa el código de envío"}
-        type={"text"}
-        name={"idNumber"}
-        value={inputValue}
-        onChange={handleChange}
-      />
-      <button className="py-2 px-4 bg-green rounded-[24px]" type="submit">
-        Buscar
-      </button>
-    </form>
+    <div>
+      <form onSubmit={onSubmit} className="space-x-3">
+        <TextInput
+          placeholdertext={"Ingresa el código de envío"}
+          type={"text"}
+          name={"idNumber"}
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button className="py-2 px-4 bg-green rounded-[24px]" type="submit">
+          Buscar
+        </button>
+      </form>
+      {errorId && <p className="text-pink ml-11">Campo Obligatorio</p>}
+    </div>
   );
 }
 
--- Client/C16-03-m-node-react-/src/views/DashboardVisitor.jsx
+++ Client/C16-03-m-node-react-/src/views/DashboardVisitor.jsx
@@ -1,23 +1,31 @@
-
-
 import HeaderNoButtons from "../components/header/HeaderNoButtons";
 import StatusBar from "../components/statusBar/StatusBar";
 import Table from "../components/tableDetails/Table";
 import { useEffect, useState } from "react";
 import services from "../services/api";
 
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function DashboardVisitor() {
   const { id } = useParams();
-
-const [products, setProducts] = useState();
+  const [products, setProducts] = useState();
+  const [errorId, setErrorId] = useState(false);
 
   useEffect(() => {
-    services.getProductData({ id: id }).then((res) => {
-      console.log(res);
-      setProducts(res);
-    });
+    services
+      .getProductData({ id: id })
+      .then((res) => {
+        console.log(res);
+        if (res && res.product._id !== id) {
+          setErrorId(true);
+        } else {
+          setProducts(res);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching product data:", error);
+        setErrorId(true);
+      });
   }, [id]);
 
   return (
@@ -25,8 +33,21 @@ const [products, setProducts] = useState();
       <HeaderNoButtons />
       <div className="flex flex-col mt-2 h-full sm:flex-row gap-5 rounded-[24px] lg:w-[90%] sm:w-3/4 min-[360px]:w-full  bg-Amethyst">
         <div className="flex flex-col w-full p-6 justify-center gap-5">
-          <StatusBar initialStatus= {products}  />
-          <Table products={products} />
+          {errorId ? (
+            <div className="flex flex-col w-full p-6 justify-center gap-5">
+              <h2 className="text-pink text-center text-lg font-bold ml-11">
+                El id no existe
+              </h2>
+              <Link className="text-green text-center" to="/">
+                Volver a inicio
+              </Link>
+            </div>
+          ) : (
+            <>
+              <StatusBar initialStatus={products} />
+              <Table products={products} />
+            </>
+          )}
         </div>
       </div>
     </div>
