db146f700fb72d4b456730fa0bed7dbb625759f2
Carlos A. Escobar <carlosesconav@gmail.com>
2024-02-28 16:54:45 -0500

    create findClientProdut route

--- Server/src/routes/product.routes.js
+++ Server/src/routes/product.routes.js
@@ -7,12 +7,14 @@ import {
   receiveProduct,
   getAllProducts,
   deleteProduct,
+  findClientProducts,
 } from '../controllers/product.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
 router.post('/createProduct', authMiddleware, createProduct);
 router.get('/getAllProducts', getAllProducts, authMiddleware);
+router.post('/findClientProducts', findClientProducts, authMiddleware);
 router.delete('/deleteProduct', deleteProduct, authMiddleware);
 router.post('/getOneProduct', getProductById);
 router.put('/updateProduct', authMiddleware, updateProduct);
