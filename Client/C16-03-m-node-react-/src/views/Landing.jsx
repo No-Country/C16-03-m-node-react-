10ba66d3c5ab7e2efb9a0145499f2686971a7b71
Esteban Sayago <sayagoo823@gmail.com>
2024-02-28 19:50:33 -0300

    add new button Dashboard

--- Client/C16-03-m-node-react-/src/views/Landing.jsx
+++ Client/C16-03-m-node-react-/src/views/Landing.jsx
@@ -1,29 +1,43 @@
 import TextLanding from "../components/textLanding/TextLanding";
 import Logo from "../components/logo/Logo";
 import LandingImage from "../components/landingImage/Image";
-import TextInput from "../components/TextInput/TextInput";
 import Button from "../components/button/button";
 import Rating from "../components/rating/Rating";
 import InitSesion from "../components/InitSesion/InitSesion";
 import { useState } from "react";
 import SearchId from "../components/searchId/SearchId";
+import useUserConfig from "../hooks/useUserConfig";
+import { useNavigate } from "react-router";
 
 function Landing() {
   const [active, setActive] = useState(false);
+  const navigate = useNavigate();
+  const { role } = useUserConfig();
 
   const handleActive = () => {
     setActive(!active);
   };
 
+  const redirect = () => {
+    navigate(`/dashboard-${dashboard}`);
+  };
+
+  const dashboard = role === "userBase" ? "admin" : "client";
+
   return (
     <main className="w-full h-full min-h-[100vh] flex justify-between items-center bg-purple flex-wrap">
       <header className="w-full p-[25px] flex justify-between items-center">
         <Logo />
-        <Button
-          text="Clientes"
-          bgcolor="bg-green"
-          onClick={() => handleActive()}
-        />
+        <div className="space-x-2">
+          {role && (
+            <Button text={"Dashboard"} bgcolor="bg-green" onClick={redirect} />
+          )}
+          <Button
+            text="Clientes"
+            bgcolor="bg-green"
+            onClick={() => handleActive()}
+          />
+        </div>
       </header>
       <section className="w-full  sm:flex sm:flex-row flex flex-col p-[25px] gap-2 justify-between items-center ">
         <div className=" p-2 sm:w-1/2 flex justify-center">
