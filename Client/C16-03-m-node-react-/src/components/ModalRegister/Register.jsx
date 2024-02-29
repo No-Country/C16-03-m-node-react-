24181ed397fc94c1fa9234f29cde8099e62a5563
Esteban Sayago <sayagoo823@gmail.com>
2024-02-29 16:26:43 -0300

    update component

--- Client/C16-03-m-node-react-/src/components/ModalRegister/Register.jsx
+++ Client/C16-03-m-node-react-/src/components/ModalRegister/Register.jsx
@@ -7,17 +7,16 @@ import { IoMdArrowBack } from "react-icons/io";
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 function RegistrationModal({ onClose, onBack }) {
-
   const handleRegister = () => {};
 
   return (
     <div className="relative flex flex-col w-1/3 h-auto bg-white rounded-3xl min-w-[360px]">
       <div className="flex flex-col items-center gap-4 p-4 py-8">
         <div className="absolute top-0 right-0 mr-4 mt-3 ">
-            <button onClick={onClose}>
-              <IoCloseCircleOutline className="text-[32px]"/>
-            </button>
-          </div>
+          <button onClick={onClose}>
+            <IoCloseCircleOutline className="text-[32px]" />
+          </button>
+        </div>
         <Logo register />
         <TextLanding
           titulo="Registro"
@@ -25,30 +24,32 @@ function RegistrationModal({ onClose, onBack }) {
           textColor="text-black"
           variant="form"
         />
-        
-        <div className="">
-          <TextInput placeholdertext={"Ingresa tu nombre"} type={"text"} />
-        </div>
-        <div className="">
-          <TextInput placeholdertext={"Ingresa tu email"} type={"email"} />
-        </div>
-        <div className="">
-          <TextInput
-            placeholdertext={"Ingresa tu contraseña"}
-            type={"password"}
-          />
-        </div>
-        <div className="flex flex-col items-center">
-          
-          <div  className="flex flex-col items-center my-2">
-            <Button text="Registrate" onClick={handleRegister} bgcolor="bg-green" />
+
+        <form action="" className="space-y-5">
+          <div className="">
+            <TextInput placeholdertext={"Ingresa tu nombre"} type={"text"} />
           </div>
-          <button onClick={onBack} className="py-2 px-4 bg-green rounded">
-            <IoMdArrowBack />
-          </button>
-        </div>
-        
-        
+          <div className="">
+            <TextInput placeholdertext={"Ingresa tu email"} type={"email"} />
+          </div>
+          <div className="">
+            <TextInput
+              placeholdertext={"Ingresa tu contraseña"}
+              type={"password"}
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <Button
+              text="Registrate"
+              onClick={handleRegister}
+              bgcolor="bg-green"
+            />
+          </div>
+        </form>
+
+        <button onClick={onBack} className="py-2 px-4 bg-green rounded">
+          <IoMdArrowBack />
+        </button>
       </div>
     </div>
   );
