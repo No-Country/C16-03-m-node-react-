201658d4ede9afb3fa2e97a021c65654cc178657
German <g44ravarotto@gmail.com>
2024-03-01 15:51:34 -0300

    worker

--- Client/C16-03-m-node-react-/src/components/header/HeaderNoButtons.jsx
+++ Client/C16-03-m-node-react-/src/components/header/HeaderNoButtons.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import Logo from "../logo/Logo";
+import Button from "../button/button";
 
 function HeaderNoButtons() {
   return (
     <div className="flex items-center w-full h-[104px] bg-bgDashboard rounded-[24px] ">
-      <div className="min-w-80 max-w-screen-lg  w-full">
-        <div className=" flex items-center p-4 justify-between w-full">
-          <Link to="/">
-            <Logo variant={"dashboard"} />
-          </Link>
-        </div>
+      <div className=" flex items-center p-4 justify-between w-full">
+        <Link to="/">
+          <Logo variant={"dashboard"} />
+        </Link>
+        <Link to={"/"}>
+          <Button text="Volver a inicio" />
+        </Link>
       </div>
     </div>
   );
