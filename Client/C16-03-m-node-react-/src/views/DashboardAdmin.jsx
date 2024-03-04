ccae7b4b0f51d2d0ca298847859e14d009b4e432
German <g44ravarotto@gmail.com>
2024-03-04 17:08:26 -0300

    WaitBackground

--- Client/C16-03-m-node-react-/src/views/DashboardAdmin.jsx
+++ Client/C16-03-m-node-react-/src/views/DashboardAdmin.jsx
@@ -9,6 +9,7 @@ import services from "../services/api";
 import Spinner from "../components/spinner/Spinner";
 import { useNavigate } from "react-router";
 import img from "../assets/worker.jpg";
+import { WaitBackground } from "../components/waitBackground/WaitBackground";
 
 function DashboardAdmin() {
   const navigate = useNavigate();
@@ -61,20 +62,24 @@ function DashboardAdmin() {
                     favor, verifique el ID e inténtelo de nuevo.
                   </h2>
                 ) : (
-                  <div className="flex flex-col p-6 justify-center items-center space-y-8">
-                    <h2 className="text-white text-center sm:text-sm lg:text-lg font-bold ml-11 ">
-                      Bienvenido al panel de control de Administrador. <br />
-                      Ingrese el Nro. de seguimiento para actualizar los
-                      detalles del paquete.
-                    </h2>
-                    <div className="sm:w-[300px] lg:w-[450px] ">
-                      <img
-                        className="rounded-[24px] object-cover"
-                        src={img}
-                        alt="trabajador"
-                      />
-                    </div>
-                  </div>
+                  // <div className="flex flex-col p-6 justify-center items-center space-y-8">
+                  //   <h2 className="text-white text-center sm:text-sm lg:text-lg font-bold ml-11 ">
+                  //     Bienvenido al panel de control de Administrador. <br />
+                  //     Ingrese el Nro. de seguimiento para actualizar los
+                  //     detalles del paquete.
+                  //   </h2>
+                  //   <div className="sm:w-[300px] lg:w-[450px] ">
+                  //     <img
+                  //       className="rounded-[24px] object-cover"
+                  //       src={img}
+                  //       alt="trabajador"
+                  //     />
+                  //   </div>
+                  // </div>
+                  <WaitBackground
+                    title="Bienvenido al panel de control de Administrador."
+                    paragraph="Ingrese el Nro. de seguimiento para actualizar los detalles del paquete."
+                  />
                 )}
               </div>
             ) : (
--- Client/C16-03-m-node-react-/src/views/DashboardClient.jsx
+++ Client/C16-03-m-node-react-/src/views/DashboardClient.jsx
@@ -6,6 +6,7 @@ import Table from "../components/tableDetails/Table";
 import NewShipment from "../components/newShipment/NewShipment";
 import api from "../services/api";
 import useUserConfig from "./../hooks/useUserConfig";
+import { WaitBackground } from "../components/waitBackground/WaitBackground";
 
 function DashboardClient() {
   const [isModalVisible, setModalVisible] = useState(false);
@@ -40,32 +41,42 @@ function DashboardClient() {
   };
 
   return (
-    <main className="w-full sm:h-[100vh] min-[360px]:h-auto bg-purpleDark  ">
-      <div className=" w-full lg:h-full sm:h-full min-[320px]:h-auto flex flex-col p-2 bg-Dark">
-        <Header openModal={handleModalOpen} />
-        <div className="w-full mt-2 h-full flex flex-col sm:flex-row gap-3">
-          <div className="lg:w-1/4 sm:w-1/2 h-full min-[360px]:w-full">
-            <MyShipments
-              products={products}
-              handleFilter={handleFilter}
-              isLoading={isLoading}
-            />
-          </div>
-          <div className="flex flex-col gap-5 lg:w-3/4 sm:w-1/2 min-[360px]:w-full justify-center bg-Amethyst rounded-[24px]  ">
-            <div className="w-full h-full flex flex-col pt-10 rounded-lg">
-              <StatusBar initialStatus={{ product: productFilter }} />
-              <Table productFilter={productFilter} />
+    console.log(products),
+    (
+      <main className="w-full sm:h-[100vh] min-[360px]:h-auto bg-purpleDark  ">
+        <div className=" w-full lg:h-full sm:h-full min-[320px]:h-auto flex flex-col p-2 bg-Dark">
+          <Header openModal={handleModalOpen} />
+          <div className="w-full mt-2 h-full flex flex-col sm:flex-row gap-3">
+            <div className="lg:w-1/4 sm:w-1/2 h-full min-[360px]:w-full">
+              <MyShipments
+                products={products}
+                handleFilter={handleFilter}
+                isLoading={isLoading}
+              />
+            </div>
+            <div className="flex flex-col gap-5 lg:w-3/4 sm:w-1/2 min-[360px]:w-full justify-center bg-Amethyst rounded-[24px]  ">
+              {  ? (
+                <div className="w-full h-full flex flex-col pt-10 rounded-lg">
+                  <StatusBar initialStatus={{ product: productFilter }} />
+                  <Table productFilter={productFilter} />
+                </div>
+              ) : (
+                <WaitBackground
+                  title="Bienvenido al panel de control para gestión de envíos!"
+                  paragraph="Crea un nuevo envío y comienza a gestionarlos"
+                />
+              )}
             </div>
           </div>
+          {isModalVisible && (
+            <NewShipment
+              reRenderProducts={getProducts}
+              handleActive={handleModalClose}
+            />
+          )}
         </div>
-        {isModalVisible && (
-          <NewShipment
-            reRenderProducts={getProducts}
-            handleActive={handleModalClose}
-          />
-        )}
-      </div>
-    </main>
+      </main>
+    )
   );
 }
 
