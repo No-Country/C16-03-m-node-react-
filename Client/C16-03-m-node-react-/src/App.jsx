9ebb4fbcb988f690e4a4852432505ac0d8e72a7b
Esteban Sayago <sayagoo823@gmail.com>
2024-03-05 14:30:32 -0300

    add useEffect to search for a local stored session

--- Client/C16-03-m-node-react-/src/App.jsx
+++ Client/C16-03-m-node-react-/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
+import useUserConfig from "./hooks/useUserConfig";
 import Routes from "./routes";
 
 function MainContainer() {
+  const { setConfig } = useUserConfig();
+
+  useEffect(() => {
+    const consult = localStorage.getItem("user");
+    if (consult) {
+      const { token, userRole } = JSON.parse(consult);
+      setConfig(token, userRole);
+    }
+  }, []);
+
   return <Routes />;
 }
 
